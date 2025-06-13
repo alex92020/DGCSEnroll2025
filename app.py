1593b9c41d5c3a4579db4863e82cd546cd9645f5
Alex Silang <alexsilang9@gmail.com>
2025-06-13 09:15:03 +0800

    Fix: host and port for Render

--- app.py
+++ app.py
@@ -311,4 +311,5 @@ def monitor():
 if __name__ == '__main__':
     if not os.path.exists(DATABASE):
         init_db()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+
